24e2bf50c060b3a5577382267e67d43309e78d94
Siddhant Gahankari <b24cm1058@iitj.ac.in>
2025-06-17 20:50:21 +0530

    Update and rename B24CS1029_B24CM1016_B24CS1069_B24CM1058_io.h to io.h

--- include/io.h
+++ include/io.h
@@ -1,6 +1,6 @@
 #ifndef IO_H
 #define IO_H
-#include "B24CS1029_B24CM1016_B24CS1069_B24CM1058_common.h"
+#include "common.h"
 
 int load_embeddings(char *filename, Embedding *embeddings); // function to load embeddings
 int load_test_pairs(char *filename); // function to load the test pairs
