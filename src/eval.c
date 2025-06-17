5290f8abf36318805999edc1264b1dea76bf4e02
Siddhant Gahankari <b24cm1058@iitj.ac.in>
2025-06-17 20:52:20 +0530

    Update and rename B24CS1029_B24CM1016_B24CS1069_B24CM1058_eval.c to eval.c

--- src/eval.c
+++ src/eval.c
@@ -1,7 +1,7 @@
-#include "../include/B24CS1029_B24CM1016_B24CS1069_B24CM1058_common.h"
-#include "../include/B24CS1029_B24CM1016_B24CS1069_B24CM1058_translate.h"
-#include "../include/B24CS1029_B24CM1016_B24CS1069_B24CM1058_eval.h"
-#include "../include/B24CS1029_B24CM1016_B24CS1069_B24CM1058_globals.h"
+#include "../include/common.h"
+#include "../include/translate.h"
+#include "../include/eval.h"
+#include "../include/globals.h"
 
 // this function evaluates the accuracy for French translations
 void evaluate_accuracy_fr() {
